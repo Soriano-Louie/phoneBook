b4fad4b2ef70774db3fb6ed71823af449920ebbe
Soriano-Louie <sorianolouie50@gmail.com>
2025-06-25 22:50:05 +0800

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
+import { fileURLToPath } from "url";
+
 const express = require("express");
 const path = require("path");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
